87bd5332dfe2c395651293a46e46850b1a693895
chienuet <nguyenchien19122000@gmail.com>
2021-07-22 14:27:44 +0700

    init project

---
+++ app/src/main/java/com/example/mealdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mealdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
